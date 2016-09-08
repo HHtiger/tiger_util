997edfd992ec9187dbbf1298771e2e5ebafc6985
hehu <872749212@qq.com>
2016-09-08 11:41:12 +0800

    pyinotify

--- pyinotify/monitor.py
+++ pyinotify/monitor.py
@@ -14,13 +14,11 @@ class MyEventHandler(pyinotify.ProcessEvent):
 
     def process_IN_ACCESS(self, event):
         print "ACCESS event:", event.pathname
-
-    logging.info("ACCESS event : %s  %s" % (os.path.join(event.path, event.name), datetime.datetime.now()))
+        logging.info("ACCESS event : %s  %s" % (os.path.join(event.path, event.name), datetime.datetime.now()))
 
     def process_IN_ATTRIB(self, event):
         print "ATTRIB event:", event.pathname
-
-    logging.info("IN_ATTRIB event : %s  %s" % (os.path.join(event.path, event.name), datetime.datetime.now()))
+        logging.info("IN_ATTRIB event : %s  %s" % (os.path.join(event.path, event.name), datetime.datetime.now()))
 
     def process_IN_CLOSE_NOWRITE(self, event):
         print "CLOSE_NOWRITE event:", event.pathname
@@ -28,37 +26,23 @@ class MyEventHandler(pyinotify.ProcessEvent):
 
     def process_IN_CLOSE_WRITE(self, event):
         print "CLOSE_WRITE event:", event.pathname
+        logging.info("CLOSE_WRITE event : %s  %s" % (os.path.join(event.path, event.name), datetime.datetime.now()))
 
+    def process_IN_CREATE(self, event):
+        print "CREATE event:", event.pathname
+        logging.info("CREATE event : %s  %s" % (os.path.join(event.path, event.name), datetime.datetime.now()))
 
-logging.info("CLOSE_WRITE event : %s  %s" % (os.path.join(event.path, event.name), datetime.datetime.now()))
+    def process_IN_DELETE(self, event):
+        print "DELETE event:", event.pathname
+        logging.info("DELETE event : %s  %s" % (os.path.join(event.path, event.name), datetime.datetime.now()))
 
+    def process_IN_MODIFY(self, event):
+        print "MODIFY event:", event.pathname
+        logging.info("MODIFY event : %s  %s" % (os.path.join(event.path, event.name), datetime.datetime.now()))
 
-def process_IN_CREATE(self, event):
-    print "CREATE event:", event.pathname
-
-
-logging.info("CREATE event : %s  %s" % (os.path.join(event.path, event.name), datetime.datetime.now()))
-
-
-def process_IN_DELETE(self, event):
-    print "DELETE event:", event.pathname
-
-
-logging.info("DELETE event : %s  %s" % (os.path.join(event.path, event.name), datetime.datetime.now()))
-
-
-def process_IN_MODIFY(self, event):
-    print "MODIFY event:", event.pathname
-
-
-logging.info("MODIFY event : %s  %s" % (os.path.join(event.path, event.name), datetime.datetime.now()))
-
-
-def process_IN_OPEN(self, event):
-    print "OPEN event:", event.pathname
-
-
-logging.info("OPEN event : %s  %s" % (os.path.join(event.path, event.name), datetime.datetime.now()))
+    def process_IN_OPEN(self, event):
+        print "OPEN event:", event.pathname
+        logging.info("OPEN event : %s  %s" % (os.path.join(event.path, event.name), datetime.datetime.now()))
 
 
 def main():
